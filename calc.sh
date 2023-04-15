81b27ba58aa189c2d4c52cc625f8d36df1004db4
ajay <ajaypanwar@gmail.com>
2023-04-15 11:35:16 +0000

    Corrected calculator script

--- calc.sh
+++ calc.sh
@@ -1,28 +1,35 @@
-# !/bin/bash
- 
-# Take user Input
-echo "Enter Two numbers : "
-read a
-read b
- 
-# Input type of operation
-echo "Enter Choice :"
-echo "1. Addition"
-echo "2. Subtraction"
-echo "3. Multiplication"
-echo "4. Division"
+#!/bin/bash
+echo "simple calculator"
+sum=0
+i="y"
+echo "enter first number"
+read n1
+echo "enter second number"
+read n2
+while [ $i = "y" ]
+do
+echo "1.Addition"
+echo "2.Subtraction"
+echo "3.Multiplication"
+echo "4.Division"
+echo "Enter choice"
 read ch
- 
-# Switch Case to perform
-# calculator operations
 case $ch in
-  1)res=`echo $a + $b | bc`
-  ;;
-  2)res=`echo $a - $b | bc`
-  ;;
-  3)res=`echo $a \* $b | bc`
-  ;;
-  4)res=`echo "scale=2; $a / $b" | bc`
-  ;;
+1)sum=$(echo " $n1 + $n2" | bc -l)
+echo "Addition is =" $sum;;
+2)sum=$(echo "$n1 - $n2" | bc -l)
+echo "Sub is =" $sum;;
+3)sum=$(echo "$n1 * $n2" | bc -l)
+echo "Mul is =" $sum;;
+4)sum=$(echo "$n1 / $n2" | bc -l)
+echo "div is =" $sum;;
+*)echo "invalid choice"
 esac
-echo "Result : $res"
+echo "Do you want to continue"
+read i
+if [ $i != "y" ]
+then
+exit
+fi
+done
+
